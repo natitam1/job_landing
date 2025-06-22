d3564bfbd525a40caf8ff83bdfcc4511c90be3c7
Sajal Talukder <sajaltalukder01@gmail.com>
2025-06-22 20:40:24 +0600

    first

---
+++ data.ts
@@ -0,0 +1,167 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+const categoryData = [
+  {
+    id: 1,
+    categoryName: "Accounting / Finance",
+    openPositions: 2,
+    // icon: <GiTakeMyMoney className="w-10 h-10 text-blue-700 dark:text-white" />,
+  },
+  {
+    id: 2,
+    categoryName: "Marketing",
+    openPositions: 86,
+    // icon: (
+    //   <IoMegaphoneOutline className="w-10 h-10 text-blue-700 dark:text-white" />
+    // ),
+  },
+  {
+    id: 3,
+    categoryName: "Design",
+    openPositions: 43,
+    // icon: (
+    //   <PiPaintBrushDuotone className="w-10 h-10 text-blue-700 dark:text-white" />
+    // ),
+  },
+  {
+    id: 4,
+    categoryName: "Development",
+    openPositions: 12,
+    // icon: <FaLaptopCode className="w-10 h-10 text-blue-700 dark:text-white" />,
+  },
+  {
+    id: 5,
+    categoryName: "Project Management",
+    openPositions: 2,
+    // icon: <LuRocket className="w-10 h-10 text-blue-700 dark:text-white" />,
+  },
+  {
+    id: 6,
+    categoryName: "Customer Service",
+    openPositions: 2,
+    // icon: (
+    //   <RiCustomerService2Fill className="w-10 h-10 text-blue-700 dark:text-white" />
+    // ),
+  },
+  {
+    id: 7,
+    categoryName: "Health and Care",
+    openPositions: 25,
+    // icon: (
+    //   <MdOutlineMedicalServices className="w-10 h-10 text-blue-700 dark:text-white" />
+    // ),
+  },
+  {
+    id: 8,
+    categoryName: "Automotive Jobs",
+    openPositions: 92,
+    // icon: (
+    //   <LiaCarSideSolid className="w-10 h-10 text-blue-700 dark:text-white" />
+    // ),
+  },
+];
+
+const CompanyData = [
+  {
+    id: 1,
+    image: "/images/c1.png",
+    name: "Udemy",
+    location: "London, UK",
+    position: "20",
+  },
+  {
+    id: 2,
+    image: "/images/c2.png",
+    name: "Stripe",
+    location: "Kolkata, India",
+    position: "30",
+  },
+  {
+    id: 3,
+    image: "/images/c3.png",
+    name: "Dropbox",
+    location: "Lahore, Pakistan",
+    position: "33",
+  },
+  {
+    id: 4,
+    image: "/images/c4.png",
+    name: "Figma",
+    location: "Dhaka, Bangladesh",
+    position: "40",
+  },
+];
+
+const jobs = [
+  {
+    id: 1,
+    image: "/images/j1.png",
+    title: "Software Engineer",
+    location: "London, UK",
+    jobType: "Full Time",
+    urgency: "Urgent",
+  },
+  {
+    id: 2,
+    image: "/images/j2.png",
+    title: "Recruiting Coordinator",
+    location: "Manchester, UK",
+    jobType: "Part Time",
+    urgency: "Urgent",
+  },
+  {
+    id: 3,
+    image: "/images/j3.png",
+    title: "Product Manager, Studio",
+    location: "Birmingham, UK",
+    jobType: "Contract",
+    urgency: "Urgent",
+  },
+  {
+    id: 4,
+    image: "/images/j4.png",
+    title: "Senior Product Designer",
+    location: "Bristol, UK",
+    jobType: "Full Time",
+    urgency: "Urgent",
+  },
+  {
+    id: 5,
+    image: "/images/j5.png",
+    title: "Product Manager, Risk",
+    location: "Edinburgh, UK",
+    jobType: "Contract",
+    urgency: "Private",
+  },
+  {
+    id: 6,
+    image: "/images/j6.png",
+    title: "Technical Architect",
+    location: "Leeds, UK",
+    jobType: "Full Time",
+    urgency: "Private",
+  },
+  {
+    id: 7,
+    image: "/images/j7.png",
+    title: "Web Developer",
+    location: "Brighton, UK",
+    jobType: "Part Time",
+    urgency: "Urgent",
+  },
+  {
+    id: 8,
+    image: "/images/j8.png",
+    title: "Senior Product Designer",
+    location: "Glasgow, UK",
+    jobType: "Contract",
+    urgency: "Private",
+  },
+  {
+    id: 9,
+    image: "/images/j9.png",
+    title: "Senior BI Analyst",
+    location: "Sheffield, UK",
+    jobType: "Full Time",
+    urgency: "Private",
+  },
+];
