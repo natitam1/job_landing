47d0bf847f21841bbe1ff72e5d2c0259028e69cb
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-06-28 23:57:40 +0300

    Set up the dynamic web page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import Home from "@/components/Home/Home";
 import React from "react";
 
 const HomePage = () => {
-  return <div className="text-9xl">HomePage</div>;
+  return;
+  <div className="overflow-hidden">
+    <Home />
+  </div>;
 };
 
 export default HomePage;
