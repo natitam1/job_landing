c49b244461d7d412b20d439c8d6ac9ef34293c51
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-07-03 11:02:40 +0300

    Finish the hero section

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -1,4 +1,5 @@
 "use client";
+import ThemeToggler from "@/components/Helper/ThemeToggler";
 import { NavLinks } from "@/constant/constant";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
@@ -23,7 +24,7 @@ const Nav = ({ openNav }: Props) => {
   }, []);
   return (
     <div
-      className={`transition-all ${
+      className={`transition-all dark:bg-gray-900 ${
         navBg ? "bg-white shadow-md" : "fixed"
       } duration-200 h-[12vh] z-[10000] fixed w-full`}
     >
@@ -31,10 +32,10 @@ const Nav = ({ openNav }: Props) => {
         <div className="flex items-center sm:space-x-20">
           {/* Logo */}
           <div className="flex items-center space-x-2 cursor-pointer">
-            <div className="w-10 h-10 bg-cyan-800 rounded-full flex items-center justify-center flex-col">
-              <LuNetwork className="w-5 h-5 text-white" />
+            <div className="w-10 h-10 bg-cyan-800 dark:bg-white rounded-full flex items-center justify-center flex-col">
+              <LuNetwork className="w-5 h-5 text-white dark:text-black" />
             </div>
-            <h1 className="text-xl hidden sm:block md:text-2xl text-cyan-800 font-bold">
+            <h1 className="text-xl hidden sm:block md:text-2xl text-cyan-800 dark:text-white font-bold">
               NatiHire
             </h1>
           </div>
@@ -45,7 +46,7 @@ const Nav = ({ openNav }: Props) => {
                 <Link
                   key={link.id}
                   href={link.url}
-                  className="text-base hover:text-cyan-700 font-medium transition-all duration-200"
+                  className="text-base hover:text-cyan-700 dark:hover:text-cyan-200 font-medium transition-all duration-200"
                 >
                   <p>{link.label}</p>
                 </Link>
@@ -56,7 +57,7 @@ const Nav = ({ openNav }: Props) => {
         {/* Buttons */}
         <div className="flex items-center space-x-4">
           {/* Login/register button */}
-          <button className="px-8 py-2.5 text-xs sm:text-sm rounded-lg cursor-pointer bg-gray-100 hover:bg-gray-300 transition-all duration-300">
+          <button className="px-8 dark:bg-gray-700 dark:hover:bg-gray-900 py-2.5 text-xs sm:text-sm rounded-lg cursor-pointer bg-gray-100 hover:bg-gray-300 transition-all duration-300">
             Login / Register
           </button>
           {/* Job post button */}
@@ -64,6 +65,7 @@ const Nav = ({ openNav }: Props) => {
             Job Post
           </button>
           {/* Theme Toggler */}
+          <ThemeToggler />
           {/* Burger menu */}
           <HiBars3BottomRight
             className="h-8 w-8 cursor-pointer text-black lg:hidden"
