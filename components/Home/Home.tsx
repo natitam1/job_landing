c61edbce07d4628365c41d8cd94382b9ac10ad3f
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-07-04 22:45:06 +0300

    Set up the hero section

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Hero from "./Hero/Hero";
 
 const Home = () => {
-  return <div className="h-[2000px]"></div>;
+  return (
+    <div className="overflow-hidden">
+      <Hero />
+    </div>
+  );
 };
 
 export default Home;
